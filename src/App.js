d77472d1ef55a8eee889c74f3b1108a8d1b14b7d
Grigoriy1209 <grigoriyv1209@gmail.com>
2024-02-01 15:56:10 +0200

    home work1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import {simsons} from "./data/dataSims";
+import {SimsComponent} from "./component/Simsons/SimsComponent";
 const App = () => {
     return (
-        <div>
-            App
+        <div className="App">
+            {
+                simsons.map((simson,id)=>(
+                         <SimsComponent item={simson} key={id}/>
+                ))
+            }
         </div>
     );
 };
-
 export {App};
