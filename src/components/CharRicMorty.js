27ea25a504640c9b3ec54fddb7899d54d925a0aa
Grigoriy1209 <grigoriyv1209@gmail.com>
2024-02-03 18:33:22 +0200

    home work1.2

--- src/components/CharRicMorty.js
+++ src/components/CharRicMorty.js
@@ -4,8 +4,8 @@ const CharRicMorty = ({item}) => {
     return (
         <div >
 
-            <h1>{item.id}{item.name}</h1>
-            <h2>{item.status}{item.species}</h2>
+            <h1>{item.id}-{item.name}</h1>
+            <h2>{item.status}-{item.species}</h2>
             <h3>{item.gender}</h3>
             <img src={item.image} alt={item.name}/>
         </div>
